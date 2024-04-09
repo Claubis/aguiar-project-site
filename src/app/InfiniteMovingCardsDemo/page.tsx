32a554b999b86312459dd6fce90e501c128ba4ae
Claudio Bispo <claudio_cssp@hotmail.com>
2024-04-08 21:01:24 -0300

    tive que alterar a extensao do arquivo infinity

--- src/app/InfiniteMovingCardsDemo/page.tsx
+++ src/app/InfiniteMovingCardsDemo/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { InfiniteMovingCards } from "../InfiniteMovingCards/page";
+import { InfiniteMovingCards } from "./InfiniteMovingCards";
 
 export default function InfiniteMovingCardsDemo() {
   return (
